acfec4f5e2f3c689d57bf82a4f8dd2a60ef381a5
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-12 20:44:04 -0300

    Melhorando as Exceptions padronizada.

--- UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -1,7 +1,21 @@
-﻿namespace UtmBuilder.Core.ValueObjects.Exceptions
+﻿using System.Text.RegularExpressions;
+
+namespace UtmBuilder.Core.ValueObjects.Exceptions
 {
     public class InvalidUrlException : Exception
     {
-        public InvalidUrlException(string message = "Invalid URL") : base(message) { }
+        private const string DefaultErrorMessage = "Invalid URL";
+        private const string UrlRegexPattern = @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
+
+        public InvalidUrlException(string message = DefaultErrorMessage) : base(message) { }
+
+        public static void ThrowIfInvalid(string address, string message = DefaultErrorMessage)
+        {
+            if (string.IsNullOrEmpty(address))
+                throw new InvalidUrlException(DefaultErrorMessage);
+
+            if (!Regex.IsMatch(address, UrlRegexPattern))
+                throw new InvalidUrlException();
+        }
     }
 }
--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -1,11 +1,9 @@
-﻿using System.Text.RegularExpressions;
-using UtmBuilder.Core.ValueObjects.Exceptions;
+﻿using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core.ValueObjects
 {
     public class Url : ValueObject
     {
-        private const string UrlRegexPattern = @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
 
         /// <summary>
         /// Create a new URL
@@ -14,8 +12,8 @@ namespace UtmBuilder.Core.ValueObjects
         public Url(string address)
         {
             Address = address;
-            if (Regex.IsMatch(Address, UrlRegexPattern))
-                throw new InvalidUrlException();
+            InvalidUrlException.ThrowIfInvalid(address);
+
 
         }
         /// <summary>
