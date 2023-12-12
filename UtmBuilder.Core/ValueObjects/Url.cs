c3fa2fb60e6358eb6339635b3f3afd6ef7a61fc1
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-12 20:35:29 -0300

    Utilizando Regex para validar URL.

--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -1,7 +1,11 @@
-﻿namespace UtmBuilder.Core.ValueObjects
+﻿using System.Text.RegularExpressions;
+
+namespace UtmBuilder.Core.ValueObjects
 {
     public class Url : ValueObject
     {
+        private const string UrlRegexPattern = @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
+
         /// <summary>
         /// Create a new URL
         /// </summary>
@@ -9,6 +13,9 @@
         public Url(string address)
         {
             Address = address;
+            if (Regex.IsMatch(Address, UrlRegexPattern))
+                throw new Exception("Teste");
+
         }
         /// <summary>
         /// Address of URL (Website link)
