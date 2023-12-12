cc68d3bfec25967bb08bdae3b601be7259e94be5
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-12 20:27:57 -0300

    Summary

--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -2,10 +2,17 @@
 {
     public class Url : ValueObject
     {
+        /// <summary>
+        /// Create a new URL
+        /// </summary>
+        /// <param name="address">Address of URL (Website link)</param>
         public Url(string address)
         {
             Address = address;
         }
+        /// <summary>
+        /// Address of URL (Website link)
+        /// </summary>
         public string Address { get; }
     }
 }
