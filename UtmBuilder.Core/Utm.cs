7d765c720a7e25b2e99ff5be9c6b2a72387339d4
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-11 23:14:50 -0300

    Organizando as Entidades

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,13 @@
+﻿namespace UtmBuilder.Core
+{
+    public class Utm
+    {
+        public string Url { get; set; }
+        public string Source { get; set; }
+        public string Medium { get; set; }
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Term { get; set; }
+        public string Content { get; set; }
+    }
+}
