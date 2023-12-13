09a3cadca0ffa30ff2506bdf63bc2d9107aa6b33
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-13 19:25:22 -0300

    Organizando o padrão para iniciar a configuração dos testes.

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,11 @@
+﻿namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class UrlTests
+    {
+        public void Dado_uma_url_invalida_deve_retornar_uma_excecao()
+        {
+
+        }
+    }
+}
