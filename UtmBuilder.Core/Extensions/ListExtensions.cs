69aabcdde0b4c283e6e7e6bef3b7b870c2529505
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-12 21:15:45 -0300

    Extentendo uma lista

--- UtmBuilder.Core/Extensions/ListExtensions.cs
+++ UtmBuilder.Core/Extensions/ListExtensions.cs
@@ -2,9 +2,10 @@
 {
     public static class ListExtensions
     {
-        public static void AddIfNotNull(this List<string> list)
+        public static void AddIfNotNull(this List<string> list, string key, string? value)
         {
-
+            if (!string.IsNullOrEmpty(value))
+                list.Add($"{key}={value}");
         }
     }
 }
--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -23,7 +23,12 @@ namespace UtmBuilder.Core
         public override string ToString()
         {
             var segments = new List<string>();
-            segments.AddIfNotNull();
+            segments.AddIfNotNull("utm_source", Campaign.Source);
+            segments.AddIfNotNull("utm_medium", Campaign.Medium);
+            segments.AddIfNotNull("utm_campaign", Campaign.Name);
+            segments.AddIfNotNull("utm_id", Campaign.Id);
+            segments.AddIfNotNull("utm_term", Campaign.Term);
+            segments.AddIfNotNull("utm_content", Campaign.Content);
 
             return $"{Url.Address}?{string.Join("&", segments)}";
         }
