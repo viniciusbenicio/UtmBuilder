abdb6dc1dfb5d1fba8c36fe4ecf5a34d6662aaa7
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-13 19:31:32 -0300

    Testando por exceção.

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -1,11 +1,29 @@
-﻿namespace UtmBuilder.Core.Tests.ValueObjects
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
 {
     [TestClass]
     public class UrlTests
     {
+        [TestMethod]
         public void Dado_uma_url_invalida_deve_retornar_uma_excecao()
         {
+            try
+            {
+                var url = new Url("xpto");
+                Assert.Fail();
+            }
+            catch (InvalidUrlException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
 
+        [TestMethod]
+        public void Dado_uma_url_valida_nao_deve_retornar_uma_excecao()
+        {
+            Assert.Fail();
         }
     }
 }
